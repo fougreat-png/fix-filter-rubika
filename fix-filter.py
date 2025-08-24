ce7cd42558a5f6b9753dc143239a91fcf032cb1e
out! <fougreat@gmail.com>
2025-08-24 14:41:25 +0330

    Update fix-filter.py

--- fix-filter.py
+++ fix-filter.py
@@ -6,8 +6,12 @@ from time import sleep
 import sys
 import datetime
 import requests
+
+# Clear the screen at the very beginning
 system("clear")
 os.system("")
+
+# Define color class for console output
 class color:
     green = "\033[32m"
     red = "\033[31m"
@@ -19,17 +23,21 @@ class color:
     mark = '\x1b[38;5;4m'
     mark1 = '\x1b[48;5;15m'
     mark2 = '\x1b[0m'
-print(color.mark+f"INSTALLING",end='')
+
+# Installation message with a loading animation
+print(color.mark + "INSTALLING", end='')
 sleep(1)
-print(color.green+f".",end='',flush=True)
+print(color.green + ".", end='', flush=True)
 sleep(1)
-print(color.green+f".",end='',flush=True)
+print(color.green + ".", end='', flush=True)
 sleep(1)
-print(color.green+f".",end='',flush=True)
+print(color.green + ".", end='', flush=True)
 sleep(1)
 print(f"\033[34m")
-banner = (f"""
- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⠃⠀⠀⠀⠀⢀⠀⣴⣿⣿⠇⠀⠀⠀⠀⠀⠀⣴⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+# Banner display (Simplified to a standard multiline string)
+banner = """
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⠃⠀⠀⠀⠀⢀⠀⣴⣿⣿⠇⠀⠀⠀⠀⠀⠀⣴⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⡇⠀⠀⢀⡰⣡⣾⣿⣿⡟⠀⠀⠀⠀⠀⢀⣴⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⡁⠀⡠⢋⣼⣿⣿⣿⢸⠁⠀⠀⠀⠀⣠⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⡿⡄⢈⣴⣿⣿⣿⣿⣽⡏⠀⠀⠀⢀⣾⣿⣿⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠃
@@ -40,8 +48,8 @@ banner = (f"""
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣰⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⡏⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⡿⣟⠋⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣟⡿⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣿⣿⡿⡿⢿⡽⠁⠀⠀⠀⠀⠀⠀⠉⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣿⣿⣿⣿⣧⣾⠂⢵⡆⠀⠀⠀⠤⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣿⣿⣿⣿⣿⡿⡿⢿⡽⠁⠀⠀⠀⠀⠀⠀⠉⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣿⣿⣿⣿⣿⣧⣾⠂⢵⡆⠀⠀⠀⠤⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣟⣞⡆⣾⠀⢀⣀⣤⣶⣦⣤⣄⣀⠠⡀⠀⠀⠀⠊⠙⠻⢿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣾⣾⣾⣿⠋⣠⣿⣿⣿⣿⣿⣿⣿⣿⢷⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣯⢹⠙⡏⢿⠁⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⣠⠶⢓⣠⣄⠀⠠⣹⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -58,25 +66,45 @@ banner = (f"""
 
 █▀▀ █ ▀▄▀   █▀▀ █ █░░ ▀█▀ █▀▀ █▀█   █▀█ █░█ █▄▄ █ █▄▀ ▄▀█
 █▀░ █ █░█   █▀░ █ █▄▄ ░█░ ██▄ █▀▄   █▀▄ █▄█ █▄█ █ █░█ █▀█ v1.1
-""")
+"""
 for bnr in banner:
-        sys.stdout.write(bnr)
-        sys.stdout.flush()
-        time.sleep(0.009)
-yty=""" 
-1.code with id 
-2.code not id
- """
-for i in yty:
-    sleep(0.05)
-    print(color.pink+i,end='',flush=True)
-    
-get=input(color.pink+'inter  number here>>> ')
+    sys.stdout.write(bnr)
+    sys.stdout.flush()
+    time.sleep(0.009)
 
-if get == 1:
-    system("clear")
-    system("python code-fix.py")
+# Main loop for the menu
+while True:
+    yty = """ 
+1. Code with ID
+2. Code without ID
+3. Exit
+ """
+    for i in yty:
+        sleep(0.01) # Reduced sleep for faster menu display
+        print(color.pink + i, end='', flush=True)
+
+    get = input(color.pink + 'Enter number here>>> ')
+
+    if get == '1':
+        system("clear")
+        print(color.blue + "Starting code with ID...")
+        system("python code-fix.py")
+        print(color.green + "Code with ID finished. Returning to menu.")
+        sleep(2) # Give user time to read message
+    elif get == '2':
+        system("clear")
+        print(color.blue + "Starting code without ID...")
+        system("python code-fix-mini.py")
+        print(color.green + "Code without ID finished. Returning to menu.")
+        sleep(2) # Give user time to read message
+    elif get == '3':
+        system("clear")
+        print(color.yellow + "Exiting the main script. Goodbye!")
+        break # Exit the while loop
+    else:
+        system("clear") # Clear before showing error and re-displaying menu
+        print(color.red + "Invalid input. Please enter 1, 2, or 3.")
+        sleep(2) # Give user time to read error message
     
-if get == 2:
+    # Clear screen before re-displaying the menu for a clean look
     system("clear")
-    system("python code-fix-mini.py")
